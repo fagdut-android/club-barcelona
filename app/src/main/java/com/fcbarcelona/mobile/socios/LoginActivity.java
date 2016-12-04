58f88682cc136caf064716d99921682151ef06b9
Facundo Scoccia <fanky10@gmail.com>
2016-12-04 19:12:30 -0300

    Initial project + LoginActivity

---
+++ app/src/main/java/com/fcbarcelona/mobile/socios/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.fcbarcelona.mobile.socios;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View v) {
+        startActivity(new Intent(this, HomeActivity.class));
+    }
+}
